d246eb281498c694c499b4742dd112d049d3132b
ANRAx <ramdassaj@gmail.com>
2019-11-24 16:30:30 -0500

    finish on-click events for selecting player and enemy, start writing on-click for attack button and gameplay logic.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -109,4 +109,54 @@ $(document).ready(function() {
         var gameMessage = $("game-message");
         gameMessage.text("");
     };
+
+    // On-Click Events 
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    // on click event for selecting our chacters
+    $("characters-section").on("click", ".chacter", function() {
+        var name = $(this).attr("data-name");
+        // if players hasn't been chosen, populate attacker with select chars. info and loop thru remaining chars and push them to combatants array
+        if(!attacker) {
+            attacker = character[names];
+            for(var key in character) {
+                if(key !== name) {
+                    combatants.push(character[key]);
+                }
+            }
+            // Hide the character select div 
+            $("#characters-section").hide();
+
+            // render our selected character and our combatants 
+            updateCharacter(attacker, "#selected-character");
+            renderEnemies(combatants);
+        }  
+    });
+
+    // on click event for each enemy
+    $("#available-to-attack-section").on("click", ".character", function() {
+        // save the opponenet name
+        var name = $(this).attr("data-name");
+
+        // if there is no defender, the clicked enemy will become the defender
+        if ($("#defender").children().length === 0) {
+            defender = characters[name];
+            updateCharacter(defender, "#defender");
+
+            // remove element (as it will now be a new defender)
+            $(this).remove();
+            clearMessage();
+        }
+    });
+
+    // on click event for attack button and gameplay logic 
+    $("#attack-button").on("click", function () {
+        if ($("#defender").children().length !== 0) {
+            var attackMessage = "You attacked " + defender.name + " for " + attacker.attack * turnCounter + " damage.";
+
+            var counterAttackMessage = defender.name + " attacked you back for " + defender.enemyAttackBack + " damage.";
+            clearMessage();
+        }
+    })
 });
+
